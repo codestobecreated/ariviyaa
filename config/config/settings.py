873decb52be316f1d12c710fa535d1f6f0f58472
codestobecreated <vijaykumarpalanisamy2001@gmail.com>
2026-09-25 15:55:30 +0530

    Configure WhiteNoise and ALLOWED_HOSTS for Render deployment

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(#i$w5mg&wdnxg4$*t0d0=0k%mhkb9po!pt%@m6zkgdui7emb4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'static',
 ]
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
